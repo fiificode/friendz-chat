da09b3acdbfac1475373e0c171935d1997557993
Franklin Mensah <84854612+fiificode@users.noreply.github.com>
2024-01-28 10:22:24 +0000

    chore:Added prisma models

--- app/(chat)/users/page.tsx
+++ app/(chat)/users/page.tsx
@@ -1,9 +1,14 @@
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, currentUser } from "@clerk/nextjs";
 import React from "react";
 
-const Users = () => {
+const Users = async () => {
+  const user = await currentUser();
   return (
     <div>
+      User: {user?.firstName}
+      createdAt: {user?.createdAt}
+      other:{user?.imageUrl}
+      others:{user?.username}
       <UserButton afterSignOutUrl="/" />
     </div>
   );
