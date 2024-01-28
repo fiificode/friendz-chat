2a94410fd764381ec740e3830d0b81c84052687a
Franklin Mensah <84854612+fiificode@users.noreply.github.com>
2024-01-28 13:22:22 +0000

    chore: added update and delete user webhook

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -64,5 +64,46 @@ export async function POST(req: Request) {
     });
   }
 
+  if (eventType === "user.updated") {
+    const currentUser = await db.user.findUnique({
+      where: {
+        externalUserId: payload.data.id,
+      },
+    });
+    if (!currentUser) {
+      return new Response("User not found", {
+        status: 404,
+      });
+    }
+    await db.user.update({
+      where: {
+        externalUserId: payload.data.id,
+      },
+      data: {
+        name: payload.data.first_name,
+        username: payload.data.username,
+        imageUrl: payload.data.image_url,
+      },
+    });
+  }
+
+  if (eventType === "user.deleted") {
+    const currentUser = await db.user.findUnique({
+      where: {
+        externalUserId: payload.data.id,
+      },
+    });
+    if (!currentUser) {
+      return new Response("User not found", {
+        status: 404,
+      });
+    }
+    await db.user.delete({
+      where: {
+        externalUserId: payload.data.id,
+      },
+    });
+  }
+
   return new Response("", { status: 200 });
 }
